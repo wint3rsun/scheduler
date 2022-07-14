7d300be7ad1039fd6b9b76796515433f629a1cee
Abigail Addae <abigail2addae@gmail.com>
2022-07-13 21:07:16 -0400

    add logic to render DayListItems given an array of days

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,9 +2,21 @@ import React from "react";
 import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
+  const items = props.days.map((dayItem) => {
+    return (
+      <DayListItem 
+      key={dayItem.id}
+      name={dayItem.name} 
+      spots={dayItem.spots} 
+      selected={dayItem.name === props.day}
+      setDay={dayItem.setDay}  
+    />
+    );
+  });
+
   return (
     <ul>
-
+      {items}
     </ul>
   );
 
