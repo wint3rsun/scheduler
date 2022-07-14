80c5452af988c9ea5093b91b083c4195b69cd878
Abigail Addae <abigail2addae@gmail.com>
2022-07-14 16:26:05 -0400

    add story for form edit and create

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 storiesOf("Button", module)
   .addParameters({
@@ -173,5 +174,22 @@ storiesOf("Appointment", module)
       message="Could not delete appointment"
       onClose={action("onClose")}
     />
-  ));
+  ))
+  .add("Form Edit", () => (
+    <Form
+      student="Abigail Addae"
+      interviewer={3}
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />
+  ))
+  .add("Form Create", () => (
+    <Form
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />
+  ))
+
 
