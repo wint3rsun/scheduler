2d1bb635a7517430406ea429796354075d4b810e
Abigail Addae <abigail2addae@gmail.com>
2022-07-14 17:35:12 -0400

    update prevent form default form submit and placeholder text

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -5,16 +5,26 @@ import Button from "components/Button";
 export default function Form(props) {
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset = () => {
+    setStudent("");
+    setInterviewer(null);
+  };
+
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  }
   
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={e => e.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
-            placeholder="Enter Student Name"
+            placeholder= {student || "Enter Student Name"}
             onChange={(e) => setStudent(e.target.value)}
           />
         </form>
@@ -26,8 +36,8 @@ export default function Form(props) {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={props.onCancel}>Cancel</Button>
-          <Button confirm onClick={props.onSave}>Save</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
+          <Button confirm onClick={() => props.onSave(student,interviewer)}>Save</Button>
         </section>
       </section>
     </main>
