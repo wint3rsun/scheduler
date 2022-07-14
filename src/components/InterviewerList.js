4c37d942e61ebd80e752eb4e85f1ae401ff6243b
Abigail Addae <abigail2addae@gmail.com>
2022-07-14 05:43:43 -0400

    add skeleton for InterviewerList component

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,11 @@
+import React from "react";
+import InterviewerListItem from "./InterviewerListItem";
+
+export default function() {
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list"></ul>
+    </section>
+  );
+}
