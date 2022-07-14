65a9616da4b27acc7d0c0c3afa2a45d25a946326
Abigail Addae <abigail2addae@gmail.com>
2022-07-14 17:11:27 -0400

    refactor to use props passed

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import InterviewerList from "components/InterviewerList";
 import Button from "components/Button";
 
-
 export default function Form(props) {
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
   
   return (
     <main className="appointment__card appointment__card--create">
@@ -14,20 +15,19 @@ export default function Form(props) {
             name="name"
             type="text"
             placeholder="Enter Student Name"
-          /*
-            This must be a controlled component
-            your code goes here
-          */
+            onChange={(e) => setStudent(e.target.value)}
           />
         </form>
         <InterviewerList
-        /* your code goes here */
+          interviewers={props.interviewers}
+          value={interviewer}
+          onChange={setInterviewer}
         />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger /* your code goes here */>Cancel</Button>
-          <Button confirm /* your code goes here */>Save</Button>
+          <Button danger onClick={props.onCancel}>Cancel</Button>
+          <Button confirm onClick={props.onSave}>Save</Button>
         </section>
       </section>
     </main>
