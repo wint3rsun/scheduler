fc3f61e356850a34d4f39007fe9bb3b2ab3b9240
Abigail Addae <abigail2addae@gmail.com>
2022-07-14 05:57:51 -0400

    refactor to display list of InterviewerListItems

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -3,11 +3,27 @@ import InterviewerListItem from "./InterviewerListItem";
 
 import "components/InterviewerList.scss"
 
-export default function() {
+//{ id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
+
+export default function(props) {
+  const items = props.interviewers.map((interviewerItem) => {
+    return (
+      <InterviewerListItem
+        id={interviewerItem.id}
+        name={interviewerItem.name}
+        avatar={interviewerItem.avatar}
+        selected={interviewerItem.id === props.interviewer}
+        setInterviewer={props.setInterviewer}
+      />
+    );
+  });
+
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">
+        {items}
+      </ul>
     </section>
   );
 }
