a66a5916b66f170fb5fc49b86509282f68b3c73e
Abigail Addae <abigail2addae@gmail.com>
2022-07-14 09:14:21 -0400

    add story for appoint show component

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,7 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
 storiesOf("Button", module)
   .addParameters({
@@ -146,6 +147,14 @@ storiesOf("Appointment", module)
   .add("Empty", () => (
     <Empty
       onAdd={action("onAdd")}
+    />
+  ))
+  .add("Show", () => (
+    <Show
+      student="Lydia Miller-Jones"
+      interviewer={interviewer}
+      onEdit={action("onEdit")}
+      onDelete={action("onDelete")}
     />
   ));
 
