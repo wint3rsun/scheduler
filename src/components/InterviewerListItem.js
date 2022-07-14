906108e45590005bb0467426d3a2f248a6c8bcb9
Abigail Addae <abigail2addae@gmail.com>
2022-07-14 05:38:53 -0400

    set <li> classes using classNames and conditionally render props.name

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,16 +1,21 @@
 import React from "react";
+import classNames from "classnames";
 
 import "components/InterviewerListItem.scss"
 
 export default function (props) {
+  const interviewerClass = classNames("interviewers__item", {
+    "interviewers__item--selected":props.selected
+  });
+
   return (
-    <li onClick={() => props.setInterviewer(props.id)} className="interviewers__item">
+    <li onClick={() => props.setInterviewer(props.id)} className={interviewerClass}>
       <img
         className="interviewers__item-image"
         src={props.avatar}
         alt={props.name}
       />
-       {props.name}
+       {props.selected && props.name}
     </li>
   );
 }
