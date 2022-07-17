fa4e63238f12ba95310cd8dc634bd7586876809d
Abigail Addae <abigail2addae@gmail.com>
2022-07-17 15:38:25 -0400

    add axios as dependency

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
+import axios from "axios";
 
 import DayList from "./DayList";
 import Appointment from "./Appointment";
