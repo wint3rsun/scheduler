e5ba4e8fd8c87ad7a7183143fee7b531008a33e7
Abigail Addae <abigail2addae@gmail.com>
2022-07-19 00:58:21 +0000

    add custome hook useVisualMode to manage appointment modes

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,32 @@
+import React, {useState} from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+  const [index, setIndex] = useState(0);
+  
+  function transition(newMode, replace = false) {
+    if(replace) {
+      setHistory([...history, history[index] = newMode]);
+      setMode(newMode);
+    } else {
+      setHistory([...history, newMode]);
+      setMode(newMode);
+      setIndex(index + 1);
+    }
+  }
+
+  function back() {  
+    setHistory([
+      ...history.slice(0,index)
+    ])
+    setIndex(index - 1);
+    setMode(history[(index > 0)?index-1:0]);
+  }
+  
+  return {
+    mode,
+    transition,
+    back
+  };
+}
