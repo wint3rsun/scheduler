ff4f8e78532cd615fc589f4d61608e18854dd49d
Abigail Addae <abigail2addae@gmail.com>
2022-07-19 19:10:25 -0400

    update Appointment with new mode SAVING to show status component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,7 +1,9 @@
 import React from "react";
+
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Status from "./Status";
 
 import "components/Appointment/styles.scss";
 
@@ -11,6 +13,7 @@ import Form from "./Form";
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
+const SAVING = "SAVING";
 
 export default function Appointment(props) {
 
@@ -21,6 +24,7 @@ export default function Appointment(props) {
       student: name,
       interviewer
     };
+    transition(SAVING);
     props.bookInterview(props.id, interview)
     .then(() => transition(SHOW));
   }
@@ -36,6 +40,7 @@ export default function Appointment(props) {
         />
       )}
       {mode === CREATE && (<Form interviewers={props.interviewers} onCancel={() => back(EMPTY)} onSave={save}/>)}
+      {mode === SAVING && (<Status message={"Saving"}/>)}
     </article>
   );
 }
