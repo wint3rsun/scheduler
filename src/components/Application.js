6be3d6a44eea5a036ba050b7a098393cd5af2e3e
Abigail Addae <abigail2addae@gmail.com>
2022-07-19 18:59:55 -0400

    update bookInterview function to send put request to persist interview into db

--- src/components/Application.js
+++ src/components/Application.js
@@ -17,22 +17,27 @@ export default function Application(props) {
   });
 
   function bookInterview(id, interview) {
-    console.log(id, interview);
-  
-    const appointment = {
-      ...state.appointments[id],
-      interview: {...interview}
-    }
-    
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    }
-    
-    setState({
-      ...state,
-      appointments
-    });
+    return axios
+    .put((`/api/appointments/${id}`), {interview})
+    .then((response) => {
+      console.log(response);
+
+      const appointment = {
+        ...state.appointments[id],
+        interview: {...interview}
+      };
+      
+      const appointments = {
+        ...state.appointments,
+        [id]: appointment
+      };
+
+      setState({
+        ...state,
+        appointments
+      });
+    })
+    .catch(err => console.log(err));
   };
 
   const appointments = getAppointmentsForDay(state, state.day);
