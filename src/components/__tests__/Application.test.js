dc264b6f9721b5d823db000d5c35d9c69c159afb
Abigail Addae <abigail2addae@gmail.com>
2022-07-21 17:00:24 -0400

    update Application test to use await

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -4,19 +4,20 @@ import { render, cleanup, waitForElement, fireEvent } from "@testing-library/rea
 
 import Application from "components/Application";
 
-import "__mocks__/axios"
+//import "__mocks__/axios"
 
 afterEach(cleanup);
 
-it("defaults to Monday and changes the schedule when a new day is selected", () => {
+it("changes the schedule when a new day is selected", async () => {
   const { getByText } = render(<Application />);
 
-  return waitForElement(() => getByText("Monday")).then(() => {
-    fireEvent.click(getByText("Tuesday"));
-    expect(getByText("Leopold Silvers")).toBeInTheDocument();
-  });
+  await waitForElement(() => getByText("Monday"));
+
+  fireEvent.click(getByText("Tuesday"));
+
+  expect(getByText("Leopold Silvers")).toBeInTheDocument();
 });
 
-it("defaults to Monday and changes the schedule when a new day is selected", () => {
-  render(<Application />);
-});
+// it("defaults to Monday and changes the schedule when a new day is selected", () => {
+//   render(<Application />);
+// });
