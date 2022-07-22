32cc11abd71f56169d73fe85dca14549643831c4
Abigail Addae <abigail2addae@gmail.com>
2022-07-21 21:02:56 -0400

    add mock delete request

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -87,6 +87,11 @@ export default {
       status: 204,
       statusText: "No Content"
     });
-
+  }),
+  delete: jest.fn((url, interview) => {
+    return Promise.resolve({
+      status: 204,
+      statusText: "No Content"
+    });
   })
 };
