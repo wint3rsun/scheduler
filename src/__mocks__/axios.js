1ea5fcbd0cd97eb9483278fb5c0d69569a08a1a9
Abigail Addae <abigail2addae@gmail.com>
2022-07-22 19:51:17 -0400

    update mock put and delete requests

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -82,13 +82,13 @@ export default {
       });
     }
   }),
-  put: jest.fn((url, interview) => {
+  put: jest.fn(() => {
     return Promise.resolve({
       status: 204,
       statusText: "No Content"
     });
   }),
-  delete: jest.fn((url, interview) => {
+  delete: jest.fn(() => {
     return Promise.resolve({
       status: 204,
       statusText: "No Content"
